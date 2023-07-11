197e720ddb528c6eef2d63e589149f942dd008f6
dariomonopoli-dev <jerryscout71@gmail.com>
2023-07-11 11:00:35 +0200

    final changes

--- src/main/java/com/example/demo/controller/ContentController.java
+++ src/main/java/com/example/demo/controller/ContentController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Content;
 import com.example.demo.repository.ContentCollectionRepository;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -24,14 +25,31 @@ public class ContentController {
         return repository.findAll();
     }
 
-    @GetMapping("{id}")
+    @GetMapping("/{id}")
     public Content findById(@PathVariable Integer id) {
         return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found"));
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
-    public void create(@RequestBody Content content) {
+    public void create(@Valid @RequestBody Content content) {
         repository.save(content);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void update(@PathVariable Integer id, @RequestBody Content content) {
+        if (!repository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found");
+        }
+
+        repository.save(content);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        repository.delete(id);
     }
 
 }
--- src/main/java/com/example/demo/model/Content.java
+++ src/main/java/com/example/demo/model/Content.java
@@ -1,11 +1,14 @@
 package com.example.demo.model;
 
+import jakarta.validation.constraints.NotBlank;
+
 
 
 import java.time.LocalDateTime;
 
 public record Content(
         Integer id,
+        @NotBlank
         String title,
         String desc,
         Status status,
